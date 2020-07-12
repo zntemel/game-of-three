c14710b0b629a6e17973ac6eb204b4d46b14c91b
Zeynep Nur Temel <zntemel@gmail.com>
2020-07-12 20:01:15 +0200

    Write App component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
+import { Route } from "react-router-dom";
+import SinglePlayer from "./containers/SinglePlayer";
+import MultiPlayer from "./containers/MultiPlayer";
+import MainScreen from "./components/MainScreen";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let pathMap = {};
+
+describe("Routes components", () => {
+  beforeAll(() => {
+    const component = shallow(<App />);
+    pathMap = component.find(Route).reduce((pathMap, route) => {
+      const routeProps = route.props();
+      pathMap[routeProps.path] = routeProps.component;
+      return pathMap;
+    }, {});
+  });
+
+  it("should show MainScreen component", () => {
+    expect(pathMap["/"]).toBe(MainScreen);
+  });
+
+  it("should show SinglePlayer component", () => {
+    expect(pathMap["/singleplayer"]).toBe(SinglePlayer);
+  });
+
+  it("should show MultiPlayer component", () => {
+    expect(pathMap["/multiplayer"]).toBe(MultiPlayer);
+  });
+
+  it("should show page not found case", () => {
+    expect(pathMap[undefined]()).toMatchObject(<div>Page not found :(</div>);
+  });
 });
