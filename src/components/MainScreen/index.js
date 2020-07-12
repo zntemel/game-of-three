95477b95769f75f2fe5e9f7785853637dadae5a5
Zeynep Nur Temel <zntemel@gmail.com>
2020-07-12 23:56:08 +0200

    Change main screen design

--- src/components/MainScreen/index.js
+++ src/components/MainScreen/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Divider, Button } from "antd";
 import "./style.scss";
 
-function MainScreen() {
+function MainScreen(props) {
   return (
     <div className="main-screen">
-      <Link to="/singleplayer">Play With Computer</Link>
-      <Link to="/multiplayer">Play With Real Player</Link>
+      <Button onClick={() => props.history.push("/singleplayer")}>Play With Computer</Button>
+      <Button onClick={() => props.history.push("/multiplayer")}>Play With a Real Player</Button>
+
+      <p className="date">Copyright ©2020</p>
+
     </div>
   );
 }
--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Divider } from "antd";
 import "./style.scss";
 
 function Layout(props) {
   return (
     <div className="container">
       <h1>Game Of Three</h1>
+      <Divider className="layout-divider"/>
       {props.children}
     </div>
   );
@@ -15,4 +17,4 @@ Layout.propTypes = {
   children: PropTypes.object,
 };
 
-export default Layout
+export default Layout;
