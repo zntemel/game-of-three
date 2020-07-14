e352d3d4734d5af58bf739ae2c9b26904cf60010
Zeynep Nur Temel <zntemel@gmail.com>
2020-07-14 17:04:51 +0300

    Create action button disable function for multiplayer game.

--- src/components/ActionComponent/index.js
+++ src/components/ActionComponent/index.js
@@ -6,13 +6,25 @@ import "./style.scss";
 function ActionComponent(props) {
   return (
     <div className="action-component">
-      <Button value={-1} onClick={() => props.actionValue(-1)}>
+      <Button
+        value={-1}
+        onClick={() => props.actionValue(-1)}
+        disabled={props.isButtonDisable}
+      >
         -1
       </Button>
-      <Button value={0} onClick={() => props.actionValue(0)}>
+      <Button
+        value={0}
+        onClick={() => props.actionValue(0)}
+        disabled={props.isButtonDisable}
+      >
         <span>0</span>
       </Button>
-      <Button value={1} onClick={() => props.actionValue(1)}>
+      <Button
+        value={1}
+        onClick={() => props.actionValue(1)}
+        disabled={props.isButtonDisable}
+      >
         <span>1</span>
       </Button>
     </div>
@@ -21,6 +33,11 @@ function ActionComponent(props) {
 
 ActionComponent.propTypes = {
   actionValue: PropTypes.func,
+  isButtonDisable: PropTypes.bool,
+};
+
+ActionComponent.defaultProps = {
+  isButtonDisable: false,
 };
 
 export default ActionComponent;
