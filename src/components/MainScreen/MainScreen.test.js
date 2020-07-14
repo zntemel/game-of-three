3568f630c038b34f4f1589f30ba7e311620e449d
Zeynep Nur Temel <zntemel@gmail.com>
2020-07-14 19:16:34 +0300

    Create Main Screen component test

---
+++ src/components/MainScreen/MainScreen.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
+import MainScreen from "./index";
+
+describe("MainScreen", () => {
+  const wrapper = shallow(<MainScreen />);
+  const instance = wrapper.instance();
+  const gameName = 'Game Of Three';
+
+  describe("shallow wrapper", () => {
+    it("should render succesfully with snapshots", () => {
+      const component = renderer.create(wrapper).toJSON();
+      expect(component).toMatchSnapshot();
+    });
+
+    it("instance should be null because of it's dumy component", () => {
+      expect(instance).toEqual(null);
+    });
+
+    it("should be include name game", () => {
+      expect(wrapper.find("h1").contains(gameName)).toBe(true);
+    });
+
+    it("should be include divider component", () => {
+      expect(wrapper.find('Divider').exists()).toBeTruthy()
+    });
+
+    it("should be include two button", () => {
+      expect(wrapper.find('Button')).toHaveLength(2);
+    });
+  });
+});
